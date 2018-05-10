68b554f2c955bb485e67fdb6156619cc88b756fc
Jhonatas Santos <contato.jhonatas17@gmail.com>
2018-05-10 19:52:45 -0300

    alteração login_steps

---
+++ mark7/Aula2/features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Dado("que acessei o formulário de login") do
+    visit '/login'
+end
+  
+Quando("faço login com {string} e {string}") do |email, senha|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então("sou autenticado com sucesso") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
