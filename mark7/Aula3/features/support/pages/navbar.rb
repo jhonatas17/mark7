1c557b5cfd79e42f438659128a578360b1656805
Jhonatas Santos <contato.jhonatas17@gmail.com>
2018-05-21 19:16:39 -0300

    no message

--- mark7/Aula3/features/support/pages/navbar.rb
+++ mark7/Aula3/features/support/pages/navbar.rb
@@ -3,7 +3,7 @@
 class Navbar < SitePrism::Page
     element :nav, '.navbar'
     element :menu_usuario, 'a[href="/user_settings dropdown-toggle"]'
-    element :link_sair, 'a[href$=logout"]'
+    element :link_sair, 'a[href$=logout]'
 
     def desloga
         menu_usuario.click
